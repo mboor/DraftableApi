a7373e3d471302cbbb6a77a6f54a86cabe25d35c
mboor <mboor03@gmail.com>
2017-03-08 18:02:37 -0600

    Add CORS Support

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 var express  = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var cors = require('cors');
+var corsOptions = {
+  origin: 'http://localhost:3000'
+}
 var players = require('./routes/players');
 var app = express();
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors(corsOptions));
 
 app.get('/', function(req, res){
   res.send('Please use /api/');
@@ -18,6 +24,6 @@ app.get('/api/players/:_id?', players.getPlayer);
 app.post('/api/players', players.createPlayer);
 app.delete('/api/players/delete/:_id', players.deletePlayer);
 app.put('/api/players/:_id?', players.updatePlayer);
-app.listen(3000, function(){
-  console.log('Server Started on Port 3000...');
+app.listen(3030, function(){
+  console.log('Server Started on Port 3030...');
 });
